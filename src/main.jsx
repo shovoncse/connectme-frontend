e08b03556f4742951be6eff93a312c2330f9b557
Shovon Das <shovon@brillmark.com>
2023-03-28 21:48:31 +0600

    login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
+import './styles/index.scss';
 import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
