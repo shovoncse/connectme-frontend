65f95bcb5fdc0b688fc0c4151440134969a9ffc3
Shovon Das <shovon@brillmark.com>
2023-04-04 14:37:32 +0600

    cleanup done - removed reactjs

--- src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomePage = () => {
-  return (
-    <h2>Home Page</h2>
-  );
-};
-
-export default HomePage;
--- src/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RegisterPage = () => {
-  return (
-    <h2>Register Page</h2>
-  );
-};
-
-export default RegisterPage;
