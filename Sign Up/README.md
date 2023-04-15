b8a2a86cc7c89cc34213c1a6cc53131d43e4af06
Mst Airen Aktar <112573038+MstAktar@users.noreply.github.com>
2023-04-15 10:58:52 +0600

    Add files via upload

---
+++ Sign Up/README.md
@@ -0,0 +1,43 @@
+# Connect Me - Social Media App
+## 🌟 Features
+- Secure user registration and authentication
+- Create posts, like and comment on posts
+- Visit user profiles/follow other users
+- Search for tweets with specific keywords
+
+## ⚙️ Tech Stack
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+
+## 🚀 Local Development
+
+Run the project in your machine locally.
+
+### Step 1: Clone the repository
+
+Clone the repo locally using:
+
+```sh
+git clone https://github.com/shovoncse/connectme-frontend.git
+```
+
+### Step 2: Install Dependencies
+
+Install dependencies in the root folder
+
+```sh
+npm install
+```
+
+### Step 3: Run the server
+
+```sh
+npm run dev
+```
+
+## Team
+
+Group 6
---
+++ Sign Up/js/register.js
@@ -0,0 +1,75 @@
+// wait for the DOM to be loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+  // check if any user already logged in
+  if(localStorage.getItem("cm-token")){
+      // if user is already logged in, redirect to home page
+      window.location.href = "../connectme-frontend/index.html";
+  } else {
+      // else continue with the registration process
+      // get the register button element
+      const registerButton = document.getElementById("register-button");
+
+      // add event listener to register button
+      registerButton.addEventListener("click", function(e) {
+          // prevent default action
+          e.preventDefault();
+          // get the values from the form
+          const name = document.getElementById("name").value;
+          const email = document.getElementById("email").value;
+          const password = document.getElementById("password").value;
+
+          // check for empty values
+          if (!name || !email || !password) {
+              alert("Please fill in all the fields.");
+              return;
+          }
+
+          // check the format of email
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(email)) {
+              alert("Please enter a valid email address.");
+              return;
+          }
+
+          // create the API request options object
+          const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password
+            })
+        };
+
+        // send the API request
+        fetch("http://localhost:3001/api/users/", requestOptions)
+            .then(response => {
+
+                if (response.ok) {
+                    // handle success
+                    alert("Registration successful.");
+                    return response.json();
+                } else {
+                    // handle errors
+                    return response.json().then(error => {
+                        throw new Error(error.message);
+                    });
+                }
+            })
+            .then(data => {
+                // save the token in local storage
+                localStorage.setItem("cm-token", data.accessToken);
+                localStorage.setItem("cm-data", JSON.stringify(data));
+                // redirect to home page
+                window.location.href = "../connectme-frontend/index.html";
+            })
+            .catch(error => {
+                alert(error.message);
+            });
+      });
+  }
+});
