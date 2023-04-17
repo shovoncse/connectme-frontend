372dd79609b938873279a94279163fa514da84d1
Ashif_Ahmed <t2moas00@students.oamk.fi>
2023-04-18 00:46:54 +0600

    js added

--- js/update-post.js
+++ js/update-post.js
@@ -62,3 +62,96 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
   loader(false);
+
+
+  // initial update form Data 
+  async function initialPostData(id) {
+
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + user.accessToken
+      }
+    };
+
+    const getPostById = await apiRequest(`http://localhost:3001/api/posts/${id}`, requestOptions);
+    if (getPostById && getPostById.status !== 404) {
+      if (getPostById.user._id !== user._id) {
+        showAlert("You are not authorized to edit this post", "error", 8000, true);
+        return;
+      }
+
+      document.getElementById("post-content").value = getPostById.postContent;
+
+      if (getPostById.image) {
+        getInitialImage(getPostById.image);
+      }
+      imageUploadInputPost('profile-upload-btn', 'profile-image-preview', 'profile-image-area');
+    } else {
+      showAlert("Post Not Found", "error", 8000, true);
+      return;
+    }
+  }
+
+  // image upload input 
+  function imageUploadInputPost(uploadBtnId, imagePreviewId, imageAreaId, postImg = '') {
+
+    const uploadBtn = document.getElementById(uploadBtnId);
+    const imagePreview = document.getElementById(imagePreviewId);
+    const deleteBtn = document.createElement('div');
+    const imageArea = document.querySelector(`.${imageAreaId}`);
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = '&times;';
+
+    uploadBtn.addEventListener('change', () => {
+      const file = uploadBtn.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        const image = document.createElement('img');
+        image.src = reader.result;
+        imagePreview.innerHTML = '';
+        imagePreview.appendChild(image);
+        imagePreview.style.display = 'block';
+        imagePreview.appendChild(deleteBtn);
+
+        // hide the image area
+        imageArea.style.display = 'none';
+      };
+    });
+
+    deleteBtn.addEventListener('click', () => {
+      imagePreview.innerHTML = '';
+      imagePreview.style.display = 'none';
+      imageArea.style.display = 'flex';
+    });
+  }
+
+  // get initial image
+  function getInitialImage(imageUrl) {
+
+    const imagePreview = document.getElementById('profile-image-preview');
+    const deleteBtn = document.createElement('div');
+    const imageArea = document.querySelector(`.profile-image-area`);
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = '&times;';
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    imagePreview.innerHTML = '';
+    imagePreview.appendChild(image);
+    imagePreview.style.display = 'block';
+    imagePreview.appendChild(deleteBtn);
+
+    // hide the image area
+    imageArea.style.display = 'none';
+
+    deleteBtn.addEventListener('click', () => {
+      imagePreview.innerHTML = '';
+      imagePreview.style.display = 'none';
+      imageArea.style.display = 'flex';
+    });
+  }
+
+
+});
