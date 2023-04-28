a0d3bde72a7de892ddf7e23c6c572a4980dbeeda
Hamidreza <hamidreza.yourdkhani1993@gmail.com>
2023-04-28 18:58:16 +0300

    type func for theme

--- js/home.js
+++ js/home.js
@@ -37,7 +37,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     } else {
         resetLocalStorage();
     }
-
     // logout button
     const logoutButton = document.getElementById("logout-button");
     logoutButton.addEventListener("click", async function (e) {
@@ -78,6 +77,102 @@ document.addEventListener("DOMContentLoaded", async function () {
             loader(false);
         }
     });
+  // Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+
+// function to open theme modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid';
+}
+
+// function to close theme modal
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none';
+  }
+}
+
+// add event listener to close theme modal when user clicks outside of it
+themeModal.addEventListener('click', closeThemeModal);
+
+// add event listener to open theme modal when user clicks on theme button
+theme.addEventListener('click', openThemeModal);
+
+// function to remove active class from all size selectors
+const removeSizeSelector = () => {
+  fontSizes.forEach(size => {
+    size.classList.remove('active');
+  });
+}
+
+// add event listeners to size selectors to change font size
+fontSizes.forEach(size => {
+  size.addEventListener('click', () => {
+    removeSizeSelector();
+    let fontSize;
+
+    // toggle active class on clicked size selector
+    size.classList.toggle('active');
+
+    // set font size and sticky top left and right values based on clicked size selector
+    if (size.classList.contains('font-size-1')) {
+      fontSize = '10px';
+      root.style.setProperty('--sticky-top-left', '5.4rem');
+      root.style.setProperty('--sticky-top-right', '5.4rem');
+    } else if (size.classList.contains('font-size-2')) {
+      fontSize = '13px';
+      root.style.setProperty('--sticky-top-left', '5.4rem');
+      root.style.setProperty('--sticky-top-right', '-7rem');
+    } else if (size.classList.contains('font-size-3')) {
+      fontSize = '16px';
+      root.style.setProperty('--sticky-top-left', '5.4rem');
+      root.style.setProperty('--sticky-top-right', '-17rem');
+    } else if (size.classList.contains('font-size-4')) {
+      fontSize = '19px';
+      root.style.setProperty('--sticky-top-left', '5.4rem');
+      root.style.setProperty('--sticky-top-right', '-19rem');
+    } else if (size.classList.contains('font-size-5')) {
+      fontSize = '22px';
+      root.style.setProperty('--sticky-top-left', '5.4rem');
+      root.style.setProperty('--sticky-top-right', '-22rem');
+    }
+
+    // change font size
+    document.querySelector('html').style.fontSize = fontSize;
+  });
+});
+// Get the color palette container element
+const colorPalette = document.querySelector('.choose-color span');
+
+// Add click event listeners to each color option
+colorPalette.forEach(color => {
+  color.addEventListener('click', () => {
+    // Define the primary hue value based on the clicked color option
+    let primaryHue;
+    if (color.classList.contains('color-1')) {
+      primaryHue = 252;
+    } else if (color.classList.contains('color-2')) {
+      primaryHue = 52;
+    } else if (color.classList.contains('color-3')) {
+      primaryHue = 352;
+    } else if (color.classList.contains('color-4')) {
+      primaryHue = 152;
+    } else if (color.classList.contains('color-5')) {
+      primaryHue = 202;
+    }
+    color.classList.add('active');
+
+    // Set the primary color hue value as a CSS variable
+    root.style.setProperty('--primary-color-hue', primaryHue);
+  });
+});
+
+
+        
+
 
 
 
